14b3d77037ab034e21de7420c474a1fd4157fae3
foodish <xbj1900@gmail.com>
2018-03-17 03:42:17 +0800

    add info

--- pingan.py
+++ pingan.py
@@ -7,6 +7,11 @@ headers={'User-Agent':'Mozilla 5.0'}
 
 
 def get_data_and_save(year_month):
+    '''
+    获取中国平安规模保费数据并保存为csv文件
+    :param year_month:形如'201801'表示18年1月份数据
+    :return:
+    '''
     url='http://www.pingan.cn/zh/common/ir/questions-answers/income/%s.shtml?' % year_month
     r=requests.get(url, headers=headers)
     r.encoding='utf-8'
@@ -16,19 +21,26 @@ def get_data_and_save(year_month):
         df.to_csv('data/pingan/%s_%s.csv' % (year_month, index), index=False, header=False)
         
 
-def start():      
+def start():
+    '''
+    下载中国平安历史规模保费数据，只需要第一次使用，后续只需下载每月数据即可
+    :return:
+    '''
     try:
         os.makedirs(r'data/pingan')
     except:
         pass  
-    year_months=[str(i)+str(j).zfill(2) for i in range(2015, 2018) for j in range(1, 13)]
+    year_months=[str(i)+str(j).zfill(2) for i in range(2015, 2019) for j in range(1, 13)]
     for i in year_months:
-        print(i)
-        get_data_and_save(i)
+        try:
+            get_data_and_save(i)
+            print(i)
+        except:
+            pass
 
 
 if __name__ == '__main__':
-    #start()
+    #start()  #第一次使用
     m = input('enter a month like 201701:')
     get_data_and_save(m)
     
